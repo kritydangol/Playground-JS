0b4c5d3d0e03e041401b8442b6cf5b994684fa8a
mediokrity <kritydangol1@gmail.com>
2023-01-23 11:33:54 +0545

    enhanced counter: added total

--- js-counter/index.js
+++ js-counter/index.js
@@ -1,6 +1,10 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let totalEl = document.getElementById("total-el")
+let message = document.getElementById("message")
 let count = 0
+
+let totalCount = 0
 
 function increment() {
     count += 1
@@ -8,8 +12,21 @@ function increment() {
 }
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
+    if (count != 0) {
+        // adding previous entries
+        let countStr = count + " - "
+        saveEl.textContent += countStr
+        // updating total
+        totalCount += count
+        totalEl.textContent = totalCount
+        // cleaning previous values
+        countEl.textContent = 0
+        count = 0
+        message.textContent = ""
+
+    }
+    else {
+        message.textContent = "Why would you want to count 0? What is wrong with you?"
+        // message.textContent = "Please increase the count before saving."
+    }
 }
