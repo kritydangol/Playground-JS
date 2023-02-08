d54a245ef9260627b869a2728ce22a6a0dc98d85
mediokrity <kritydangol1@gmail.com>
2023-02-08 23:03:27 +0545

    blackjack button littttle bit functional

--- js-blackjack/index.js
+++ js-blackjack/index.js
@@ -1,5 +1,19 @@
+let firstCard = 10
+let secondCard = 1
+
+let cardsEl = document.getElementById("cards-el")
+let sumEl = document.getElementById("sum-el")
+// cardsEl.textContent
 function startGame(){
-    console.log("Game Has Started")
+    renderGame()
+}
+
+function renderGame(){
+    console.log(cardsEl.textContent)
+    console.log(sumEl.textContent)
+    cardsEl.textContent = "Cards:" + " " + firstCard + " " + secondCard
+    let sum = firstCard + secondCard
+    sumEl.textContent = "Sum:" + " " + sum
 }
 
 function newCard(){
