f12cb752de871173a405508c0dd5bc9662ed2768
mediokrity <kritydangol1@gmail.com>
2023-03-23 13:13:49 +0545

    chrome extension started

---
+++ js-chrome-extension/index.js
@@ -0,0 +1,9 @@
+// Refactor the code so that it uses .addEventListener()
+// when you click the SAVE INPUT button
+
+let inputBtnEl = document.getElementById("input-btn")
+
+inputBtnEl.addEventListener("click", function () {
+    console.log("Button clicked!")
+})
+
