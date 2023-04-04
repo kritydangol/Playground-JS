fc1209cf33ed4187b5e1843494535d204e1950cf
mediokrity <kritydangol1@gmail.com>
2023-04-04 12:36:25 +0545

    deployment chrome extension

--- js-chrome-extension/index.js
+++ js-chrome-extension/index.js
@@ -5,6 +5,7 @@ const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
+    console.log(myLeads)
     inputEl.value = ""
     renderLeads()
 })
@@ -12,14 +13,13 @@ inputBtn.addEventListener("click", function () {
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
         listItems += `
-                    <li>
-                        <a target='_blank' href='${myLeads[i]}'>
-                            ${myLeads[i]}
-                        </a>
-                    </li>
-                    `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
     }
     ulEl.innerHTML = listItems
 }
