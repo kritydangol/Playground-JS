ea3260c0c1119db42c2c542a1873c036d9bbd992
mediokrity <kritydangol1@gmail.com>
2023-04-11 13:13:07 +0545

    make the delete button work: double click

--- js-chrome-extension/index.js
+++ js-chrome-extension/index.js
@@ -2,17 +2,23 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-
-// ["lead1", "lead2"] or null
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-
-// 1. Check if leadsFromLocalStorage is truthy
-// 2. If so, set myLeads to its value and call renderLeads()
+const deleteBtn = document.getElementById("delete-btn")
+// 1. Store the delete button in a deleteBtn variable
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     renderLeads()
 }
+
+// 2. Listen for double clicks on the delete button (google it!)
+// 3. When clicked, clear localStorage, myLeads, and the DOM
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
@@ -34,7 +40,6 @@ function renderLeads() {
     }
     ulEl.innerHTML = listItems
 }
-
 
 // note: local storage use:
 // localStorage.setItem("key","value")
