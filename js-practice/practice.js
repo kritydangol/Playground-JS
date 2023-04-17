c03ef44782236b4067f1a4d4b16c8a6abc4add82
mediokrity <kritydangol1@gmail.com>
2023-04-17 12:31:13 +0545

    PRACTICE: 1 & 2

--- js-practice/practice.js
+++ js-practice/practice.js
@@ -125,3 +125,50 @@ function getRandom() {
     return hands[randomIndex]
 }
 console.log(getRandom())
+
+
+
+
+// _____________________________________________________________________
+// part 3 | 1
+// LET & CONST
+
+// SETTING THE STAGE
+const player = "Per"
+const opponent = "Nick"
+const game = "AmazingFighter"
+let points = 0
+let hasWon = false
+
+// PLAYING THE GAME
+points += 100
+hasWon = true
+
+// ANNOUNCING THE WINNER
+if (hasWon) {
+    console.log(`${player} got ${points} points and won the ${game} game!`)
+} else {
+    console.log(`The winner is ${opponent}! ${player} lost the game`)
+}
+
+// Go through all variables and decide if they should be let or const
+// Change the console logs to use template strings instead of double quotes
+
+
+// _____________________________________________________________________
+// part 3 | 2
+// LOG OUT ITEMS IN ARRAY
+
+let myCourses = ["Learn CSS Animations", "UI Design Fundamentals", "Intro to Clean Code"]
+
+// Create a function that takes a single parameter, an array,
+// and logs all the items of the array to the console.
+// Call the function while passing in myCourses as an argument
+
+function logItems(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i])
+    }
+}
+
+arrayThing(myCourses)
