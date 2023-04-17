ffe9990130c9cb3733542f90c5b2d5d0b0626dcd
mediokrity <kritydangol1@gmail.com>
2023-04-17 13:07:09 +0545

    practice: 4

--- js-practice/practice.js
+++ js-practice/practice.js
@@ -184,3 +184,29 @@ arrayThing(myCourses)
 localStorage.setItem("thing", "wow")
 console.log(localStorage.getItem("thing"))
 localStorage.clear()
+
+
+// _____________________________________________________________________
+// part 3 | 4
+// addEventListener() and object in array
+
+let data = [
+    {
+        player: "Jane",
+        score: 52
+    },
+    {
+        player: "Mark",
+        score: 41
+    }
+]
+
+// Fetch the button from the DOM, store it in a variable
+// Use addEventListener() to listen for button clicks
+// Log Jane's score when the button is clicked (via data)
+
+const janeEl = document.getElementById("jane")
+
+janeEl.addEventListener("click", () => {
+    console.log(data[0].score)
+})
